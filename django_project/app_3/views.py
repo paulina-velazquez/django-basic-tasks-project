1ce65cb9b82ede89f8a3eb2e8754c8158c56d07c
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-03-25 11:36:03 -0400

    add app3

--- django_project/app_3/views.py
+++ django_project/app_3/views.py
@@ -1,21 +1,35 @@
 from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 
-tasks = ["foo", "bar", "baz"]
 
 
 class NewTaskForm(forms.Form):
-    tasks = forms.CharField(label="New Task")
+    task = forms.CharField(label="New Task")
 
 
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "app_3/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 
 def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("app_3:index"))
+        else:
+            return render(request, "app_3/add.html", {
+                "form": form
+            })
+
     return render(request, "app_3/add.html", {
         "form": NewTaskForm()
     })
